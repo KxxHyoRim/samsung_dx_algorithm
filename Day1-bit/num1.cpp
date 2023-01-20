c791d0f9c73747f552d9f23c3d520870ee40d262
kxxhyorim <rlagyfla0709@gmail.com>
2023-01-20 19:45:16 +0900

    [Day1] num1 (bit masking)

---
+++ Day1-bit/num1.cpp
@@ -0,0 +1,37 @@
+/* bit연산
+ * num1. 새로운 불면증 치료법
+ * */
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int T, n;
+    //freopen("num1.txt", "r", stdin);
+
+    cin >> T;
+
+    for (int t = 1; t <= T; t++) {
+        cin >> n;
+        int bit = 0b0;
+        int mul = 1;
+        int kN;
+        while (bit < 0b1111111111) {
+            kN = 0b1 * n * mul++;
+            while(kN != 0){
+                int num = kN%10;
+                kN /= 10;
+                bit |= (1<<num);
+            }
+        }
+        cout << "#" << t << " " << 0b1 * n * (mul-1) << "\n";
+    }
+
+    return 0;
+}
