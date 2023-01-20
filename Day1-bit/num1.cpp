3d78d76818aa961c4fa66fc34ef97517a8d86c6e
kxxhyorim <rlagyfla0709@gmail.com>
2023-01-21 00:17:48 +0900

    [Day1] num1 - edit configuration 수정을 통한 freopen작동 성공

--- Day1-bit/num1.cpp
+++ Day1-bit/num1.cpp
@@ -7,13 +7,13 @@
 using namespace std;
 
 int main() {
+    freopen("num1.txt", "r", stdin);
 
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
 
     int T, n;
-    //freopen("num1.txt", "r", stdin);
 
     cin >> T;
 
