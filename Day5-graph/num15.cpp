cdafe609b83dcaa5fc6d9c53322340244e53a1fd
kxxhyorim <rlagyfla0709@gmail.com>
2023-02-05 00:41:38 +0900

    [Day5] num15 (다른사람 코드 / 추후 분석해보기)

--- Day5-graph/num15.cpp
+++ Day5-graph/num15.cpp
@@ -1,104 +1,107 @@
 #include <iostream>
-#include <list>
+#include <algorithm>
+#include <vector>
+#include <cstring>
 #include <queue>
-
-#define ll long long
-
 using namespace std;
+#define ll long long
+const int MAXN = 100005;
+int n;
+int d[MAXN],parent[MAXN][23];
+bool check[MAXN];
+vector<vector<int>> Graph;
 
-int TC, N, parentIdx;
-list<int> pList[100000];
-list<int> visited;
-int depth[100000];
-int parent[100000][21];
-
-ll bfs();
-
-int LCA(int from, int to);
-
-void dfs(int, int);
-
-void setParent();
-
-int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-    freopen("num15.txt", "r", stdin);
-
-    cin >> TC;
-    for (int tc = 1; tc <= TC; tc++) {
-        pList->clear();
-        visited.clear();
-        fill(depth, depth + N, 0);
-        memset(parent, 0, sizeof(parent));
-
-        depth[1] = 0;
-        cin >> N;
-        for (int i = 2; i < N + 1; i++) {
-            cin >> parentIdx;
-            parent[i][0] = parentIdx;
-            depth[i] = depth[parentIdx] + 1;
-            pList[parentIdx].push_back(i);
-        }
-
-        setParent();
-        cout << "#" << tc << " " << bfs() << "\n";
-    }
-    return 0;
-}
-
-ll bfs() {
-    long long move = 0;
-    int n = 1;
-    int from = n;
-    queue<int> q;
-    q.emplace(n);
-    visited.push_back(n);
-    int tempDepth = 0;
-    while (!q.empty()) {
-        n = q.front();
-        int to = n;
-        int lca = LCA(from, to);
-        move += depth[from] + depth[to] - 2 * depth[lca];
-        q.pop();
-        tempDepth++;
-        while (!pList[n].empty()) {
-            int child = pList[n].front();
-            pList[n].pop_front();
-            q.emplace(child);
-            visited.push_back(child);
-        }
-        from = to;
-    }
-    return move;
-}
-
-void setParent() {
-    for (int i = 1; i <= 20; i++) {
-        for (int j = 0; j < N; j++) {
+void sparse(){
+    for(int i=1;i<=20;i++){
+        for(int j=0;j<n;j++){
             parent[j][i] = parent[parent[j][i - 1]][i - 1];
         }
     }
 }
 
-int LCA(int from, int to) {
-    if (depth[from] < depth[to])
-        swap(from, to);
-
-    for (int i = 19; i >= 0; i--) {
-        if ((depth[from] - depth[to]) >= (1 << i))
-            from = parent[from][i];
+int lca(int x,int y){
+    if(d[y]>d[x]) swap(x,y);
+    for(int i=19;i>=0;i--){
+        if((d[x]-d[y])>=(1<<i)) x = parent[x][i];
     }
-
-    if (from == to)
-        return from;
-
-    for (int i = 0; i <= 19; i++) {
-        if (parent[from][i] != parent[to][i]) {
-            from = parent[from][i];
-            to = parent[to][i];
+    if(x==y) return x;
+    for(int i=19;i>=0;i--){
+        if(parent[x][i] != parent[y][i]){
+            x = parent[x][i];
+            y = parent[y][i];
         }
     }
-    return parent[from][0];
+    return parent[x][0];
 }
+
+int main(){
+    //freopen("input.txt","r",stdin);
+    int tcase; scanf(" %d",&tcase); int c=1;
+    while(tcase--){
+        memset(check,0,sizeof(check));
+        memset(d,0,sizeof(d));
+        memset(parent, 0, sizeof(parent));
+        Graph.clear();
+
+        scanf(" %d",&n);
+        Graph.resize(n+3);
+
+        for(int i=1;i<n;i++){
+            int x; scanf(" %d",&x);
+            Graph[x-1].push_back(i);
+        }
+        for(int i=0;i<n;i++) sort(Graph[i].begin(),Graph[i].end());
+
+        queue<pair<int,int>> dq;
+        dq.push({0,0});
+        check[0]=true;
+
+        while(!dq.empty()){
+            int now = dq.front().first;
+            int depth = dq.front().second;
+            dq.pop();
+            d[now]=depth;
+
+            for(int i=0;i<Graph[now].size();i++){
+                int next = Graph[now][i];
+                if(!check[next]){
+                    check[next]=true;
+                    parent[next][0]=now;
+                    dq.push({next,depth+1});
+                }
+            }
+        }
+
+        sparse();
+
+        ll ret=0;
+        memset(check,0,sizeof(check));
+        vector<int> vt;
+        queue<int> q;
+        q.push(0);
+        check[0]=true;
+
+        while(!q.empty()){
+            int now = q.front();
+            q.pop();
+
+            vt.push_back(now);
+            for(int i=0;i<Graph[now].size();i++){
+                int next = Graph[now][i];
+                if(!check[next]){
+                    check[next]=true;
+                    q.push(next);
+                }
+            }
+        }
+        for(int i=0;i<n-1;i++){
+            int LCA = lca(vt[i],vt[i+1]);
+            ll a = d[vt[i]] - d[LCA];
+            ll b = d[vt[i+1]] - d[LCA];
+            ret+=a; ret+=b;
+        }
+
+        printf("#%d %lld\n",c++,ret);
+    }
+}
+
